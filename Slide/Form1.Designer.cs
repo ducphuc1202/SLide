45d757b144766e2dbd73626f4e89a5d25d8452ef
DUC PHUC <nkcm.mua@gmail.com>
2019-09-20 06:09:28 +0700

    Initial commit

---
+++ Slide/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Slide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.slideContainer = new System.Windows.Forms.PictureBox();
+            this.btnImage = new System.Windows.Forms.LinkLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.slideContainer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // slideContainer
+            // 
+            this.slideContainer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.slideContainer.Location = new System.Drawing.Point(43, 21);
+            this.slideContainer.Name = "slideContainer";
+            this.slideContainer.Size = new System.Drawing.Size(728, 294);
+            this.slideContainer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.slideContainer.TabIndex = 0;
+            this.slideContainer.TabStop = false;
+            // 
+            // btnImage
+            // 
+            this.btnImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImage.AutoSize = true;
+            this.btnImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnImage.LinkColor = System.Drawing.Color.White;
+            this.btnImage.Location = new System.Drawing.Point(698, 354);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Padding = new System.Windows.Forms.Padding(10);
+            this.btnImage.Size = new System.Drawing.Size(73, 33);
+            this.btnImage.TabIndex = 1;
+            this.btnImage.TabStop = true;
+            this.btnImage.Text = "Chon anh";
+            this.btnImage.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.BtnImage_LinkClicked);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnImage);
+            this.Controls.Add(this.slideContainer);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slide Image";
+            ((System.ComponentModel.ISupportInitialize)(this.slideContainer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox slideContainer;
+        private System.Windows.Forms.LinkLabel btnImage;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Slide/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slide
+{
+    public partial class Form1 : Form
+    {
+        Image img;
+        string pathDirectoryImg;
+        string pathAvatarImg;
+        private string openFileName;
+        List<string> imageFiles = new List<string>();
+        int thutu = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            slideContainer.AllowDrop = true;
+            pathDirectoryImg = Application.StartupPath + "/img";
+            if (Directory.Exists(Application.StartupPath + "/img"))
+            {
+                var files = Directory.GetFiles(Application.StartupPath + "/img", "*.*", SearchOption.AllDirectories);
+                if(files.Length > 0)
+                {
+                    foreach (string filename in files)
+                    {
+                        if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+                        {
+                            imageFiles.Add(filename);
+                        }
+                    }
+                    FileStream fileStream = new FileStream(imageFiles[0], FileMode.Open, FileAccess.Read);
+                    slideContainer.Image = Image.FromStream(fileStream);
+                    fileStream.Close();
+                }
+
+            }
+            else
+            {
+                slideContainer.Image = Properties.Resources.design_form_SV;
+            }
+
+        }
+
+        private void BtnImage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            OpenFileDialog openfileDialog = new OpenFileDialog();
+            openfileDialog.Title = "Chon anh dai dien";
+            openfileDialog.Filter = "File anh(*.png; *.jpg)|*.png; *.jpg";
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                openFileName = openfileDialog.FileName;
+                try
+                {
+                    var files = Directory.GetFiles(folderDialog.SelectedPath, "*.*", SearchOption.AllDirectories);
+
+                    imageFiles = new List<string>();
+                    int count = 1;
+                    
+                    foreach (string filename in files)
+                    {
+                        if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+                        {
+                            imageFiles.Add(filename);
+                            
+                        }
+                            
+
+                    }
+                    if (imageFiles.Count > 0) {
+                        timer1.Stop();
+                        if (Directory.Exists(Application.StartupPath + "/img"))
+                        {
+                            var old_files = Directory.GetFiles(Application.StartupPath + "/img", "*.*", SearchOption.AllDirectories);
+                            if (old_files.Length > 0)
+                            {
+                                foreach (string filename in old_files)
+                                {
+                                    if (Regex.IsMatch(filename, @".jpg|.png|.gif$"))
+                                    {
+                                        
+                                        File.Delete(filename);
+                                    }
+                                }
+                            }
+                              
+    
+                            
+                        }
+                            
+                        foreach (string filename in imageFiles)
+                        {
+                            img = Image.FromFile(filename);
+                            if (img != null)
+                            {
+                                if (!Directory.Exists(pathDirectoryImg))
+                                {
+                                    Directory.CreateDirectory(pathDirectoryImg);
+                                }
+                                
+                                string a = String.Format("/slide_{0}.png", count);
+                                img.Save(pathDirectoryImg + a);
+                                count++;
+
+                            }
+                        }
+                            
+                    }
+
+                    MessageBox.Show("Upload thanh cong. Anh moi se duoc load sau 3s",
+                                "Thong bao",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                    thutu = 0;
+                    timer1.Start();
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("An error occurred while attempting to load the file. The error is:"
+                                    + System.Environment.NewLine + exp.ToString() + System.Environment.NewLine);
+                    
+                }
+                Invalidate();
+            }
+        }
+
+        
+
+  
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if(imageFiles.Count > 0)
+            {
+                //slideContainer.Image = Image.FromFile(imageFiles[thutu]);
+                FileStream fileStream = new FileStream(imageFiles[thutu], FileMode.Open, FileAccess.Read);
+                slideContainer.Image = Image.FromStream(fileStream);
+                fileStream.Close();
+                if (thutu == imageFiles.Count - 1)
+                    thutu = 0;
+                else
+                    thutu++;
+            }
+            
+            
+        }
+    }
+}
